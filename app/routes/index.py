d12ad4dfd96ffea6de69c81c7a632bf644952deb
Moosa <computerknowledge817@gmail.com>
2023-04-17 00:26:45 +0500

    modified:   app/routes/index.py

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
-
+import config
 
 templates = Jinja2Templates(directory="app/templates")
 
@@ -10,6 +10,7 @@ index_app = APIRouter()
 
 @index_app.get("/")
 async def home(request: Request):
+    print(config.client_id, config.client_secret)
     context = {"request": request}
     return templates.TemplateResponse("home.html", context=context)
 
